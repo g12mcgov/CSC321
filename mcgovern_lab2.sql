bc71302a1528d6acdc766ad31e7ba3ab92f15e36
Grant McGovern <grant@cobrain.com>
2015-09-21 15:35:32 -0400

    Adding #5

--- mcgovern_lab2.sql
+++ mcgovern_lab2.sql
@@ -2,13 +2,16 @@
 * @Author: grantmcgovern
 * @Date:   2015-09-20 13:57:58
 * @Last Modified by:   grantmcgovern
-* @Last Modified time: 2015-09-20 14:34:10
+* @Last Modified time: 2015-09-21 15:31:21
 */
 
 /*
 1.
 */
-SELECT MAX(Invoice.Total), MIN(Invoice.Total) FROM Invoice;
+SELECT MAX(Invoice.Total) AS Totals 
+FROM Invoice 
+	UNION 
+	SELECT MIN(Invoice.Total) FROM Invoice;
 
 /*
 2.
@@ -17,7 +20,7 @@ SELECT ALbum.Title FROM Album
 JOIN 
 	Track USING(AlbumId) 
 	GROUP BY Album.Title 
-		HAVING COUNT(*)=13;
+		HAVING COUNT(*) = 13;
 
 /*
 3.
@@ -32,12 +35,33 @@ JOIN
 /*
 4.
 */
+/* First run this command */
 CREATE VIEW AlbumLengthView AS
-SELECT Album.Title, TOTAL(Track.Milliseconds) AS Length FROM Album 
+SELECT Album.Title AS Title, TOTAL(Track.Milliseconds) AS Length FROM Album 
 JOIN 
 	Track USING(AlbumId) 
 	GROUP BY Album.Title;
-SELECT MAX(Length) FROM AlbumLengthView;
+/* Then run the below command */
+SELECT Title, MAX(Length) FROM AlbumLengthView;
+
+/*
+5.
+*/
+/*
+	SKIP FOR NOW
+*/
+
+/*
+6.
+*/
+SELECT Track.Name FROM Track
+EXCEPT
+	SELECT Track.Name FROM 
+		Track JOIN PlaylistTrack USING(TrackId);
+
+/*
+7.
+*/
 
 
 
